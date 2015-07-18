ad9638b1e71a45e3331dd64f19f3a19c145d5ff2
Niels Laukens <niels.laukens@vrt.be>
2015-07-18 09:49:39 +0200

    Parser, 3nd try

--- vobsub.pl
+++ vobsub.pl
@@ -3,136 +3,217 @@
 use strict;
 use warnings;
 
+use Carp;
 use Data::ParseBinary;
+
 use Data::Dumper;
 
 sub hexdump ($) {
 	return join(' ', map { sprintf "%02x", ord($_) } split //, $_[0]);
 }
 
+{
+	package DS::PES;
+	our @ISA = qw{Data::ParseBinary::Stream::Reader};
 
-open my $fh, "<:raw", $ARGV[0] or die("Couldn't open: $!");
-my $stream = CreateStreamReader(File => $fh);
-my $pos = $stream->tell;
+	sub isBitStream { return 0 };
 
-my %spu_buf;
-while(1) {
-	$pos = $stream->tell;
-	my $header = Bytes("sync", 3)->parse($stream);
-	if( $header ne "\x00\x00\x01" ) {
-		printf STDERR "\@0x%04x Expected PES header, but got: %s\n",
-			$pos, hexdump($header);
-		exit 1;
+	sub new {
+		my ($class, $source) = @_;
+		my $self = {
+			debug => 0,
+			state => 'init',
+			source => $source,
+			pos => 0,
+			datalen => undef,
+		};
+		return bless $self, $class;
 	}
-	$pos = $stream->tell;
-	my $streamid = Byte("stream id")->parse($stream);
 
-	if( $streamid == 0xba ) {
-		my $ph = BitStruct("Pack header",
-				Const( BitField("marker", 2), 1 ),
-				BitField("SCR[32..30]", 3),
-				Bit("mbs_1"),
-				BitField("SCR[29..15]", 15),
-				Bit("mbs_2"),
-				BitField("SCR[14..0]", 15),
-				Bit("mbs_3"),
-				BitField("SCR ext", 9),
-				#Value("SCR", sub { ((($_->ctx->{"SCR[32..30]"} << 30) |
-				#                     ($_->ctx->{"SCR[29..15]"} << 15) |
-				#                     ($_->ctx->{"SCR[14..0]"}  <<  0)
-				#                    ) * 300 + $_->ctx->{"SCR ext"}
-				#                   ) / 27000000.} ), # seconds
-				Bit("mbs_4"),
-				BitField("bitrate", 22),
-				BitField("mbs_5", 2),
-				BitField("reserved", 5),
-				BitField("stuffing length", 3),
-				Bytes("padding", sub { $_->ctx->{"stuffing length"} } ),
-			)->parse($stream);
+	sub ReadBytes {
+		my ($self, $count) = @_;
 
-	} elsif( $streamid == 0xbd ) {
-		my $length = UBInt16("length")->parse($stream);
-		my $startpos = $stream->tell;
-		my $extensions = BitStruct("extensions",
-				Const( BitField("magic", 2), 2),
-				BitField("PES scrambling code", 2),
-				Bit("PES priority"),
-				Bit("data alignment indicator"),
-				Bit("copyright"),
-				Bit("original"),
+		my $buf = '';
 
-				Bit("PTS present"),
-				Bit("DTS present"),
-				Const( Bit("ESCR present"), 0 ),
-				Const( Bit("ES rate present"), 0 ),
-				Const( Bit("DSM trick mode"), 0 ),
-				Const( Bit("additional copy info present"), 0 ),
-				Const( Bit("PES CRC present"), 0 ),
-				Const( Bit("PES extension present"), 0 ),
+		while( length $buf < $count ) {
+			if( $self->{state} eq 'init' ) {
+				# read new packet from underlying stream
+				my $sync = ::Bytes("sync", 3)->parse($self->{source});
+				if( $sync ne "\x00\x00\x01" ) {
+					::croak $self->vtell . " Expected PES sync, but got: " . ::hexdump($sync);
+				}
 
-				Byte("PES header data length"),
-				If( sub { $_->ctx->{"PTS present"} }, BitStruct("PTS",
-					# 5 bytes
-					Const( BitField("magic", 4), 0x2 ),
-					BitField("PTS[32..30]", 3),
-					Bit("mbs_1"),
-					BitField("PTS[29..15]", 15),
-					Bit("mbs_2"),
-					BitField("PTS[14..0]", 15),
-					Bit("mbs_3"),
-				)),
-				If( sub { $_->ctx->{"DTS present"} }, BitStruct("DTS",
-					# 5 bytes
-					Const( BitField("magic", 4), 0x2 ),
-					BitField("DTS[32..30]", 3),
-					Bit("mbs_1"),
-					BitField("DTS[29..15]", 15),
-					Bit("mbs_2"),
-					BitField("DTS[14..0]", 15),
-					Bit("mbs_3"),
-				)),
-			)->parse($stream);
-		$length -= $stream->tell - $startpos;
-		my $substreamid = Byte("substream ID")->parse($stream);
-		$pos = $stream->tell;
-		my $spu_data = Bytes("PES data", $length - 1 )->parse($stream);
-		$spu_buf{$substreamid} .= $spu_data;
+				my $streamid = ::Byte("")->parse($self->{source});
+				printf STDERR "\@0x%x PES header streamID 0x%02x\n", $self->{source}->tell-4, $streamid if $self->{debug};
 
-		while( length($spu_buf{$substreamid}) >= 2 ) {
-			my $spulen = unpack "n", $spu_buf{$substreamid};
-			if( $spulen == 0 ) {
-				substr($spu_buf{$substreamid}, 0, 2, '');
+				if( $streamid == 0xba ) {
+					::Bytes("", 10)->parse($self->{source});
 
-			} elsif( length($spu_buf{$substreamid}) >= $spulen ) {
-				parse_spu( substr($spu_buf{$substreamid}, 0, $spulen, ''), $pos );
-				printf "%d remaining\n", length($spu_buf{$substreamid});
+				} elsif( $streamid == 0xbe ) {
+					my $length = ::UBInt16("")->parse($self->{source});
+					::Bytes("", $length)->parse($self->{source});
+
+				} elsif( $streamid == 0xbd ) {
+					my $length = ::UBInt16("")->parse($self->{source});
+					my $pos = $self->{source}->tell;
+					$self->{pos} = 0;
+					my $extensions = ::BitStruct("extensions",
+							::Const( ::BitField("magic", 2), 2),
+							::BitField("PES scrambling code", 2),
+							::Bit("PES priority"),
+							::Bit("data alignment indicator"),
+							::Bit("copyright"),
+							::Bit("original"),
+
+							::Bit("PTS present"),
+							::Bit("DTS present"),
+							::Const( ::Bit("ESCR present"), 0 ),
+							::Const( ::Bit("ES rate present"), 0 ),
+							::Const( ::Bit("DSM trick mode"), 0 ),
+							::Const( ::Bit("additional copy info present"), 0 ),
+							::Const( ::Bit("PES CRC present"), 0 ),
+							::Const( ::Bit("PES extension present"), 0 ),
+
+							::Byte("PES header data length"),
+							::If( sub { $_->ctx->{"PTS present"} }, ::BitStruct("PTS",
+								# 5 bytes
+								::Const( ::BitField("magic", 4), 0x2 ),
+								::BitField("PTS[32..30]", 3),
+								::Bit("mbs_1"),
+								::BitField("PTS[29..15]", 15),
+								::Bit("mbs_2"),
+								::BitField("PTS[14..0]", 15),
+								::Bit("mbs_3"),
+							)),
+							::If( sub { $_->ctx->{"DTS present"} }, ::BitStruct("DTS",
+								# 5 bytes
+								::Const( ::BitField("magic", 4), 0x2 ),
+								::BitField("DTS[32..30]", 3),
+								::Bit("mbs_1"),
+								::BitField("DTS[29..15]", 15),
+								::Bit("mbs_2"),
+								::BitField("DTS[14..0]", 15),
+								::Bit("mbs_3"),
+							)),
+							::Bytes("padding", sub { $_->ctx->{"PES header data length"}
+							                         - ($_->ctx->{"PTS present"} ? 5 : 0)
+							                         - ($_->ctx->{"DTS present"} ? 5 : 0)
+							                       } ),
+						)->parse($self->{source});
+
+					my $substreamid = ::Byte("substream ID")->parse($self->{source});
+					$length -= $self->{source}->tell - $pos; # Remove extensions & substreamid
+
+					if( $substreamid != 0x20 ) {
+						::carp $self->vtell . " Substream ID not 0x20 but $substreamid";
+						::Bytes("pes data", $length )->parse( $self->{source} );
+
+					} else {
+						printf STDERR "\@0x%x %d = 0x%x bytes of data\n", $self->{source}->tell, $length, $length if $self->{debug};
+						$self->{datalen} = $length;
+						$self->{state} = 'data';
+					}
+
+				} else {
+					::croak $self->vtell . " Unknown stream ID $streamid";
+				}
+
+			} elsif( $self->{state} eq 'data' ) {
+				# Read the requested amounts of bytes, up to the amount available
+				# in this PES
+				my $avail = $self->{datalen} - $self->{pos};
+				my $toread = $count - length($buf);
+				if( $avail < $toread ) { $toread = $avail; }
+				printf STDERR "\@0x%x reading %d = 0x%x bytes\n", $self->{source}->tell, $toread, $toread if $self->{debug};
+				$buf .= ::Bytes("pes data", $toread )->parse( $self->{source} );
+				printf STDERR "\@0x%x read %d = 0x%x / %d = 0x%x bytes\n", $self->{source}->tell, length($buf), length($buf), $count, $count if $self->{debug};
+				$self->{pos} += $toread;
+
+				if( $self->{pos} == $self->{datalen} ) {
+					$self->{state} = 'init';
+				}
 
 			} else {
-				last;
+				::confess "Invalid state " . $self->{state} . "\n";
 			}
 		}
 
-	} elsif( $streamid == 0xbe ) {
-		my $length = UBInt16("length")->parse($stream);
-		Bytes("data", $length)->parse($stream); # padding
-
-	} else {
-		printf STDERR "\@0x%04x Unknown stream ID 0x%02x\n", $pos, $streamid;
-		exit 1;
+		return $buf;
 	}
+
+	sub next_pes {
+		my ($self) = @_;
+		my $newpos = $self->{source}->tell + ($self->{datalen} - $self->{pos});
+		$self->{source}->seek($newpos);
+		$self->{state} = 'init';
+	}
+
+	sub ReadBits {
+		my ($self, $bitcount) = @_;
+		return $self->_readBitsForByteStream($bitcount);
+	}
+
+	sub tell {
+		my $self = shift;
+		return $self->{pos};
+	}
+
+	sub vtell {
+		my $self = shift;
+		return sprintf("PES\@0x%x parent\@0x%x", scalar($self->tell), $self->{source}->tell );
+	}
+
+	#sub seek {
+	#	my ($self, $newpos) = @_;
+	#}
 }
 
+open my $fh, "<:raw", $ARGV[0] or die("Couldn't open: $!");
+my $s_file = CreateStreamReader(File => $fh);
+my $s_pesdata = DS::PES->new($s_file);
 
-sub parse_spu {
-	my $stream = CreateStreamReader(StringRef => \$_[0]);
+while(1) {
+	my $length = UBInt16("length")->parse($s_pesdata);
+	next if $length == 0;
 
-	my $length = UBInt16("")->parse($stream);
-	my $data_end = UBInt16("")->parse($stream);
-
+	my $data_end = UBInt16("data end")->parse($s_pesdata) + 2;
 	if( $data_end > $length ) {
-		printf STDERR "\@0x%04x SPU: data_end (0x%04x) larger than total length (0x%04x)\n", $_[1], $data_end, $length;
-		exit 1;
+		printf STDERR "%s SPU: data_end (0x%04x) larger than total length (0x%04x)\n", $s_pesdata->vtell, $data_end, $length;
 	}
 
-	printf "\@0x%04x +0x%x SPU\n", $_[1], $length;
+	my $data = Bytes("data", $data_end - 4)->parse($s_pesdata);
+
+	my $control_end = UBInt16("control end")->parse($s_pesdata);
+	if( $control_end <= $data_end ) {
+		printf STDERR "%s SPU: control_end (0x%04x) smaller than data_end (0x%04x)\n", $s_pesdata->vtell, $control_end, $data_end;
+	} elsif( $control_end > $length ) {
+		printf STDERR "%s SPU: control_end (0x%04x) larger than total length (0x%04x)\n", $s_pesdata->vtell, $control_end, $length;
+	}
+
+	my $control = Bytes("control", $control_end - $data_end - 2)->parse($s_pesdata);
+
+	my $duration = UBInt16("duration")->parse($s_pesdata);
+	my $control_end2 = UBInt16("control end")->parse($s_pesdata);
+	if( $control_end != $control_end2 ) {
+		printf STDERR "%s SPU: control_end (0x%04x) != control_end2 (0x%04x)\n", $s_pesdata->vtell, $control_end, $control_end2;
+	}
+
+	{
+		my $two = Byte("0x02")->parse($s_pesdata);
+		if( $two != 0x02 ) {
+			printf STDERR "%s SPU: end sequence (0x%02x) != 0x02\n", $s_pesdata->vtell, $two;
+		}
+		my $ff = Byte("0x02")->parse($s_pesdata);
+		if( $ff != 0xff ) {
+			printf STDERR "%s SPU: end sequence (0x%02x) != 0xff\n", $s_pesdata->vtell, $ff;
+		}
+		if( $control_end % 2 ) {
+			$ff = Byte("0x02")->parse($s_pesdata);
+			if( $ff != 0xff ) {
+				printf STDERR "%s SPU: end sequence (0x%02x) != 0xff\n", $s_pesdata->vtell, $ff;
+			}
+		}
+	}
+
+	printf "%s +0x%x SPU\n", $s_pesdata->vtell, $length;
 }
